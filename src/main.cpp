19e922a0f576b45a54854e445cfbf54ca6f58e63
JPcGaM3 <jaitniphat@gmail.com>
2024-11-25 21:53:57 +0700

    feat: soilmoisture & water pump without test

--- src/main.cpp
+++ src/main.cpp
@@ -13,11 +13,11 @@
 #include <Adafruit_GFX.h>
 
 // Wi-Fi credentials
-const char *ssid = "punchpnp";
-const char *password = "0955967996";
+const char *ssid = "Jorpor.foto";
+const char *password = "88888888";
 
 // Server details
-const char *serverAddress = "172.20.10.3"; // CHANGE TO ESP32#2'S IP ADDRESS
+const char *serverAddress = "172.20.10.7"; // CHANGE TO ESP32#2'S IP ADDRESS
 const int serverPort = 80;
 
 WiFiClient TCPclient;
@@ -25,6 +25,8 @@ WiFiClient TCPclient;
 // Ultrasonic sensor pins
 const int trigPin = 13; // Trigger pin
 const int echoPin = 12; // Echo pin
+const int soilMoistPin = 35;
+const int relayPin = 27;
 
 void setup()
 {
@@ -32,6 +34,8 @@ void setup()
   // Set up ultrasonic sensor pins
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  pinMode(soilMoistPin, INPUT);
+  pinMode(relayPin, OUTPUT);
 
   // Connect to Wi-Fi
   WiFi.begin(ssid, password);
@@ -59,61 +63,119 @@ void setup()
 }
 
 bool ultrasonicEnabled = false; // ULtrasonic Function
+bool waterPumpEnabled = false;
+int soilMoistValue = 0;
+
+void soilMoist()
+{
+  soilMoistValue = (100.00 - ((analogRead(soilMoistPin) / 4095.00) * 100.00));
+  Blynk.virtualWrite(V1, soilMoistValue);
+
+  if (TCPclient.connected())
+  {
+    if (soilMoistValue < 50)
+    {
+      TCPclient.print("water");
+      TCPclient.print("\n");
+      Serial.println("\"water\" sent to server.");
+    }
+
+    // Wait for the server response
+    if (TCPclient.available())
+    {
+      String response = TCPclient.readStringUntil('\n');
+      Serial.print("Response from server: ");
+      if (response == "water")
+      {
+        Serial.print("Response from server: ");
+        Serial.println(response);
+        waterPumpEnabled = true;
+      }
+    }
+  }
+  else
+  {
+    Serial.println("Not connected to server.");
+  }
+  delay(1000);
+}
+
+void waterPump()
+{
+  Serial.println("WaterPump Start");
+  delay(2000);
+  Serial.println("WaterPump Strop");
+  waterPumpEnabled = false;
+}
+
+void Ultrasonic()
+{
+  // Measure distance using ultrasonic sensor
+  long duration;
+  float distance;
+
+  // Send a 10-microsecond pulse to Trig
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Read the Echo pin
+  duration = pulseIn(echoPin, HIGH);
+
+  // Calculate distance in cm (sound speed = 343 m/s)
+  distance = (duration * 0.0343) / 2;
+
+  // Print the distance to the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  // Send the distance value to the server
+  if (TCPclient.connected())
+  {
+    // TCPclient.print("Distance: ");
+    TCPclient.print(distance);
+    // TCPclient.println(" cm");
+    TCPclient.print("\n");
+    Serial.println("Distance sent to server.");
+
+    // Wait for the server response
+    if (TCPclient.available())
+    {
+      String response = TCPclient.readStringUntil('\n');
+      Serial.print("Response from server: ");
+      Serial.println(response);
+    }
+  }
+  else
+  {
+    Serial.println("Not connected to server.");
+  }
+  delay(1000);
+}
 
 void loop()
 {
   Blynk.run();
 
-  if (ultrasonicEnabled)
+  soilMoist();
+
+  if (!ultrasonicEnabled || !waterPumpEnabled)
   {
-    // Measure distance using ultrasonic sensor
-    long duration;
-    float distance;
-
-    // Send a 10-microsecond pulse to Trig
-    digitalWrite(trigPin, LOW);
-    delayMicroseconds(2);
-    digitalWrite(trigPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
-
-    // Read the Echo pin
-    duration = pulseIn(echoPin, HIGH);
-
-    // Calculate distance in cm (sound speed = 343 m/s)
-    distance = (duration * 0.0343) / 2;
-
-    // Print the distance to the Serial Monitor
-    Serial.print("Distance: ");
-    Serial.print(distance);
-    Serial.println(" cm");
-
-    // Send the distance value to the server
-    if (TCPclient.connected())
-    {
-      // TCPclient.print("Distance: ");
-      TCPclient.print(distance);
-      // TCPclient.println(" cm");
-      TCPclient.print("\n");
-      Serial.println("Distance sent to server.");
-
-      // Wait for the server response
-      if (TCPclient.available())
-      {
-        String response = TCPclient.readStringUntil('\n'); 
-        Serial.print("Response from server: ");            
-        Serial.println(response);                          
-      }
-    }
-    else
-    {
-      Serial.println("Not connected to server.");
-    }
-    delay(1000);
+    delay(500);
   }
   else
   {
-    delay(500); 
+    if (ultrasonicEnabled)
+    {
+      Ultrasonic();
+    }
+    if (waterPumpEnabled)
+    {
+      waterPump();
+    }
   }
 }
 
